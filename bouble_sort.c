a9e3ac085480c4f2ed99952b708ec5151e341e15
Vica <victoria.remm@gmail.com>
2022-04-13 15:32:28 +0300

    First release of Hello World!

---
+++ bouble_sort.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void mass(int *i, int o){
+
+	int temp = 0;
+	int n = 1;
+
+	while(n != o){
+		for (int j = 1; j != o; j++){
+			if (i[j] > i[j+1]){
+				temp = i[j];
+				i[j] = i[j+1];
+				i[j+1] = temp;
+			}
+		}
+		n++;
+	}
+
+	printf("\nSorted\n");
+	printf("\nKakashka\n");
+
+	for (int j = 1; j != o; j++)
+		printf("%d ", i[j]);
+	}
+
+int main(int argc, char *argv[]){
+
+	int a[argc];
+	printf("Unity count %d\n", argc);
+	printf("\nList mass\n");
+
+	for (int i = 1; i < argc; i++){
+		a[i] = atoi(argv[i]);
+		printf("%d ", a[i]);
+	}
+	printf("\n");
+
+	mass(a, argc);
+
+	printf("\n");
+}
